89ddb2f90c0b4021ea8aa9414198ea580cefc400
David Jeffrey <mail@davidjeffrey.co.uk>
2016-07-18 17:54:49 +0000

    Escape quote marks in attributes

--- test/test.ml
+++ test/test.ml
@@ -23,11 +23,18 @@ let test_nested test_ctx =
          [ p [] [ text "Hello, world!" ] ]
        ] |> render_html)
     "<html><body><p>Hello, world!</p></html>" 
+    
+let test_escape_attr test_ctx =
+  assert_equal
+    ~printer: identity
+    "<p class=\"This should be &quot;escaped&quot;\"></p>"
+    (p [class' "This should be \"escaped\""] [] |> render_html)
 
 let test_suite =
   "hoquet_tests" >:::
     [ "test_attributes" >:: test_attributes;
-      "test_script'"     >:: test_script' ]
+      "test_script'"     >:: test_script';
+      "test_escape_attr" >:: test_escape_attr ]
 
 let () =
   run_test_tt_main test_suite
